263c2ec015443b4f42f16a3d1b23ae4fd53d3eb2
ljyj115 <kimyeji4176@gmail.com>
2024-03-22 15:15:34 +0900

    파일이동

--- datahub_front/src/project/components/uI/HistoryButtonUI.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "../styles.module.css";
-
-export default function HistoryButtonUI({title}){
-    return(
-        <div className={styles.HistoryBtns}>
-            <div className={styles.HistoryBtn}>{title}</div>
-        </div>
-    )
-}
